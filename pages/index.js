2d643e468a918aee9230cde354e2c66b35e8539a
Mandeep kaur <89207752+mandeepkaur53@users.noreply.github.com>
2022-12-27 10:16:08 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,92 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+
+
+export default function Home() {
+  return (
+    // <div className={styles.container}>
+    <div>
+      <Head>
+        <title>Welcome To Food Blog</title>
+        
+      </Head>
+
+      <main>
+       <div className={styles.main}>
+       <h1>One Page Love</h1> 
+       </div>
+        
+       <div className={styles.one}>
+        <h1>One Page One Page Blog Websites</h1>
+        <p>A curated collection of 10 one page blog websites  for inspiration and references.<br></br> 
+        Each review includes a full screenshot of the website design along with noteworthy<br></br> 
+        features. These are real niche One Page blogs that promote simplicity and faster <br></br>
+        UX with AJAX loading content within the same page. Additionally, they do not <br></br>reload the page – keeping it a One Page website.</p>
+       
+       
+        <h2>Get New inspiration</h2>
+        <a href="https://thenomadsoffantasy.com/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+         <button>INSPIRATION NEWSLETTER</button> </a>
+       
+       </div>
+       
+        <div className={styles.card1}>
+            <div>
+            <a href="https://thenomadsoffantasy.com/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+            <img src='https://onepagelove.imgix.net/2022/11/opl-screenshot-32.jpg?w=600&max-h=800&fit=crop&fp-y=0&auto=compress'></img></a>
+            </div>           
+          
+            <div >
+            <a href="https://www.myrch.club/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+            <img src='https://onepagelove.imgix.net/2021/08/opl-master-27.jpg?w=600&max-h=800&fit=crop&fp-y=0&auto=compress'></img></a>
+            </div>
+          
+            <div >
+            <a href="https://www.myrch.club/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+            <img src='https://onepagelove.imgix.net/2021/08/opl-master-26.jpg?w=600&max-h=800&fit=crop&fp-y=0&auto=compress'></img></a>
+            </div>
+
+            <div >
+            <a href="https://www.myrch.club/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+            <img src='https://onepagelove.imgix.net/2020/02/opl-master.jpg?w=600&max-h=800&fit=crop&fp-y=0&auto=compress'></img></a>
+            </div>
+
+          <div >
+            <a href="https://www.myrch.club/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+            <img src='https://onepagelove.imgix.net/2017/11/opl-big-19.jpg?w=600&max-h=800&fit=crop&fp-y=0&auto=compress'></img></a>
+            </div>
+
+          <div >
+            <a href="https://www.myrch.club/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+            <img src='https://onepagelove.imgix.net/2017/09/opl-big-5.jpg?w=600&max-h=800&fit=crop&fp-y=0&auto=compress'></img></a>
+            </div>
+
+          <div >
+            <a href="https://www.myrch.club/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+            <img src='https://onepagelove.imgix.net/2022/11/opl-screenshot-32.jpg?w=600&max-h=800&fit=crop&fp-y=0&auto=compress'></img></a>
+            </div>
+
+          <div >
+            <a href="https://www.myrch.club/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+            <img src='https://onepagelove.imgix.net/2022/12/unfold-reels-thumb-long-1.jpg?w=600&max-h=800&fit=crop&fp-y=0&auto=compress'></img></a>
+            </div>
+
+          <div >
+            <a href="https://www.myrch.club/?ref=onepagelove" target="_blank" title="Launch Website" rel="nofollow">
+            <img src='https://onepagelove.imgix.net/2022/11/opl-screenshot-32.jpg?w=600&max-h=800&fit=crop&fp-y=0&auto=compress'></img></a>
+            </div>
+
+        </div>
+
+        
+
+      
+
+       
+
+       
+      </main>
+      </div>
+
+   )
+  }
